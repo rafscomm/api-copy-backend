67d7614ea0123bda43c46fe11baad792120d4141
Rafscomm <rafaelpereira2033@gmail.com>
2023-01-23 16:01:00 -0300

    Criação inicial de rotas, variaveis de ambientes

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,9 @@
-import express from 'express'
-
+import express from 'express';
+import {router} from './routes';
+import 'dotenv/config';
 const server = express();
 
-server.get('/', (req, res)=>{
-  return res.send('Teste')
-})
+server.use(router);
+server.use(express.json());
 
-
-export {server}
+export {server};
---
+++ src/server/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {StatusCodes} from 'http-status-codes';
+
+const router = Router();
+
+router.get('/', (_, res)=>{
+  return res.send('Teste');
+});
+router.post('/teste', (req, res)=>{
+  console.log(req.body);
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+export {router};
