455b3222635e785bb698516b1f2437873b1dde1a
Rafscomm <rafaelpereira2033@gmail.com>
2023-01-23 14:09:54 -0300

    Configuracoes de eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import {server} from './server/Server'
+import { server } from './server/Server';
 
-server.listen(3333, ()=> console.log('Esta rodando'))
+server.listen(3333,
+  () => console.log('Esta rodando'));
